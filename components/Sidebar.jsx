ff1bc2b5649b2c7ef7b5563dbb07932abcd6ded5
xndrbrgs <aborgescolon@gmail.com>
2022-05-22 12:32:31 -0400

    creating sidebar with buttons

---
+++ components/Sidebar.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { HomeIcon, SearchIcon, LibraryIcon, PlusCircleIcon, HeartIcon, RssIcon } from '@heroicons/react/outline';
+
+function Sidebar() {
+  return (
+    <div className='text-gray-500 p-5 text-sm border-r border-gray-900'>
+        <div>
+          <button className='flex items-center space-x-2 hover:text-white'>
+            <HomeIcon className='h-5 w-5' />
+            <p>Home</p>
+          </button>
+          <button className='flex items-center space-x-2 hover:text-white'>
+            <SearchIcon className='h-5 w-5' />
+            <p>Search</p>
+          </button>
+          <button className='flex items-center space-x-2 hover:text-white'>
+            <LibraryIcon className='h-5 w-5' />
+            <p>Your Library</p>
+          </button>
+          <hr className='border-t-[0.1px] border-gray-900'/>
+
+          <button className='flex items-center space-x-2 hover:text-white'>
+            <HeartIcon className='h-5 w-5' />
+            <p>Create Playlist</p>
+          </button>
+          <button className='flex items-center space-x-2 hover:text-white'>
+            <RssIcon className='h-5 w-5' />
+            <p>Your Episodes</p>
+          </button>
+          <button className='flex items-center space-x-2 hover:text-white'>
+            <LibraryIcon className='h-5 w-5' />
+            <p>Your Library</p>
+          </button>
+          <hr className='border-t-[0.1px] border-gray-900'/>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
